5c07cefa1214c66bd9d72fca24e5834f693cb326
da99 <i-hate-spam-1234567@mailinator.com>
2017-05-25 12:46:53 -0500

    Added: is-younger-than

---
+++ bin/public/is-younger-than/_.sh
@@ -0,0 +1,20 @@
+
+source "$THIS_DIR"/bin/public/modified-age/_.sh
+
+# === {{CMD}}  SECS  path/to/file
+# === Exits with 1 if file is older.
+# === Exits with 0 if file is younger or does not exist.
+is-younger-than () {
+  local +x SECONDS="$1"; shift
+  local +x FILENAME="$1"; shift
+
+  if [[ ! -e "$FILENAME" ]]; then
+    return 0
+  fi
+
+  local +x NOW="$(date +"%s")"
+  local +x THEN="$(stat -c %Y "$FILENAME")"
+  local +x AGE="$(( NOW - THEN ))"
+
+  [[ "$AGE" -lt "$SECONDS" ]]
+} # === end function
