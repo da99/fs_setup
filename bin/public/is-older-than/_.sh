35000e96a00c85f2ddf159936613d2197448cdfd
da99 <i-hate-spam-1234567@mailinator.com>
2017-05-25 12:58:49 -0500

    Added: tips

--- bin/public/is-older-than/_.sh
+++ bin/public/is-older-than/_.sh
@@ -2,7 +2,8 @@
 source "$THIS_DIR"/bin/public/modified-age/_.sh
 
 # === {{CMD}}  SECONDS  filename
-# === Exits with 1 if file is old or does not exist.
+# === Exits with 1 if file is young or does not exist.
+# === Easier to reason if you combine it with: `test -e "filename"`
 is-older-than () {
   local +x SECONDS="$1"; shift
   local +x FILENAME="$1"; shift
--- bin/public/is-younger-than/_.sh
+++ bin/public/is-younger-than/_.sh
@@ -3,7 +3,8 @@ source "$THIS_DIR"/bin/public/modified-age/_.sh
 
 # === {{CMD}}  SECS  path/to/file
 # === Exits with 1 if file is older.
-# === Exits with 0 if file is younger or does not exist.
+# === Exits with 0 if file is young or does not exist.
+# === Easier to reason if you combine it with: `test -e "filename"`
 is-younger-than () {
   local +x SECONDS="$1"; shift
   local +x FILENAME="$1"; shift
