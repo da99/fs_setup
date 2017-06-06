245b19523e22f099aedca6d92f65d4f2b1838658
da99 <i-hate-spam-1234567@mailinator.com>
2017-06-05 19:25:04 -0500

    Fixed: resolve source, but not destination.

--- bin/public/link/_.sh
+++ bin/public/link/_.sh
@@ -1,53 +1,46 @@
 
 # === {{CMD}}  source(old)  dest(new)
+# === `ln -s` does not resolve source destinations.
+# === For example: config/file -> dir/file -> (non-existant) dir/config/file
+# === This function resolves the source before linking.
+# === It does not resolve the DEST because DEST can already exist and point
+# === to a non-existing location, which would be captured by a resolve.
 link () {
   PATH="$PATH:$THIS_DIR/../sh_color/bin"
-  local +x orig=$1
-  local +x target=$2
+  local +x ORIG="$(realpath --canonicalize-existing "$1")"
+  local +x DEST="$2"
 
-  local +x REAL_ORIG="$(realpath "$orig")"
-  local +x REAL_TARGET="$(realpath "$target")"
-
-  if [[ -d "$REAL_TARGET" ]]; then
-    local +x REAL_TARGET_DIR="$REAL_TARGET"
-  else
-    local +x REAL_TARGET_DIR="$(dirname "$REAL_TARGET")"
+  if [[ -z "$ORIG" || -z "$DEST" ]]; then
+    exit 2
   fi
 
-  # Is this a file being linked to a directory?:
-  #   ln -s file dir/
-  if [[ -f "$REAL_ORIG" && -d "$REAL_TARGET"  && "$REAL_TARGET" == "$REAL_TARGET_DIR" ]]; then
-    REAL_TARGET="$REAL_TARGET/$(basename "$REAL_ORIG")"
+  if [[ -d "$DEST" ]]; then
+    DEST="$DEST/$(basename "$ORIG")"
+  fi
+
+  # === Check if it already exists:
+  if [[ "$ORIG" == "$(realpath --canonicalize-missing "$DEST")" ]]; then
+    echo "=== Already linked:" >&2
+    echo "$ORIG -> $DEST" >&2
+    return 0
+  fi
+
+  if [[ -h "$DEST"  && ! -e "$DEST" ]]; then
+    echo "!!! Link exists, but is broken: " >&2
+    echo "$DEST -> "$(realpath --canonicalize-missing "$DEST")""
+    exit 2
   fi
 
   local +x CMD="ln -s"
-  if [[ "$(stat -c %U "$REAL_TARGET_DIR" )" != "$USER" ]]; then
+  if [[ "$(stat -c %U "$(dirname "$DEST")" )" != "$USER" && ! -w "$(dirname "$DEST")" ]]; then
     CMD="sudo $CMD"
   fi
 
-  # IF broken link exists, remove
-  if [[ -h "$target" && ! -e "$REAL_TARGET" ]]; then
-    echo -e "\n!!! trashing broken link: $target\n"
-    trash-put $target
-  fi
-
   # --- IF it does NOT exit?
-  if [[ ! -e "$REAL_TARGET" ]] ; then # --- create link
-    echo "=== Running: $CMD $orig $target" >&2
-    $CMD $orig $target
-    sh_color GREEN "--- Linked: $orig {{$target}}"
-  else
+  echo "=== Running: $CMD $ORIG $DEST" >&2
+  $CMD "$ORIG" "$DEST"
+  sh_color GREEN "--- Linked: $ORIG {{$DEST}}"
 
-    # --- IF is a sym link?
-    if [[ "$(realpath "$REAL_TARGET")" == "$REAL_ORIG" ]]; then
-      echo "Already sym link: $target"
-
-    else
-      sh_color RED -e "\n=== Check existing object: {{$target}}"
-      echo -e "\n diff $orig $target\n\n" 1>&2
-      exit 1
-    fi # --- valid sym link?
-  fi # --- ! -e
 } # === end function
 
 
