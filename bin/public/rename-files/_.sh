0c440123a7a9c14da8e8143ddfea3272d53b692a
da99 <i-hate-spam-1234567@mailinator.com>
2017-06-08 13:17:09 -0500

    Changed: rename-files to be more interactive.

--- bin/public/rename-files/_.sh
+++ bin/public/rename-files/_.sh
@@ -1,10 +1,24 @@
 
-# === {{CMD}}  partial-to-remove
-# === "cd" into a directory and remove a partial substring from each filename.
+# === {{CMD}}  list of files ...
+# === Interactive programs: asks you what to rename each file to.
 rename-files () {
-  local +x PARTIAL="$@"
-  local +x IFS=$'\n'
-  for FILE in $(find . -mindepth 1 -maxdepth 1 -type f); do
-    mv -i "$FILE" "${FILE/"$PARTIAL"/}"
+  while [[ ! -z "$@" ]]; do
+    local +x FILE="$1"; shift
+    local +x REAL_FILE="$(realpath "$FILE")"
+    local +x REAL_DIR="$(dirname "$REAL_FILE")"
+    echo -n "Change $REAL_FILE to new file name: "
+    read -r NEW_NAME
+    echo -n "Confirm (y/n): $REAL_FILE -> $REAL_DIR/$NEW_NAME : "
+    read -r ANS
+    local +x -l ANS="$ANS"
+
+    case "$ANS" in
+      y|yes)
+        mv -i "$REAL_FILE" "$REAL_DIR"/"$NEW_NAME"
+        ;;
+      *)
+        continue
+        ;;
+    esac
   done
 } # === end function
